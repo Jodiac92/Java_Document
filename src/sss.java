78fd8a5e6fb93013b62e00431affb8eb45647480
Jodiac92 <fracasado5243@gmail.com>
2019-07-31 12:54:45 +0900

    2019-07-31

---
+++ src/sss.java
@@ -0,0 +1,18 @@
+
+public class sss {
+	int a;
+	String s; // 멤버변수 => 클래스 내부에 있는 변수
+	
+	public sss() {
+		
+	}// 생성자 => 객체를 생성하거나 컴파일될때 자동생성
+
+	public void aaa() {
+		
+	} // 반환을 하지 않는 메서드 => return값이 없다.
+	
+	public int aa() {
+		int a = 0;
+		return a;
+	}// int형식을 반환하는 메서드 => int형식 메서드면 return하는 값도 int형식
+}
